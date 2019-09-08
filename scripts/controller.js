a1bea917d9f1e1ccfea26e545d44c91e0f6999be
Heavenjes <paulinecalatrava@gmail.com>
2019-09-08 19:25:01 +0800

    ready for online deployment

--- scripts/controller.js
+++ scripts/controller.js
@@ -18,7 +18,7 @@ var test;
 $('#btn-connect').on('click', function () {
   // connect
   console.log("connect button clicked..")
-  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt")
   $("#status").text("Connecting....")
   $("#status").css("color", "yellow")
   $("#status").css("font-style", "italic")
